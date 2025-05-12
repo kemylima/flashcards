36597508f76a1b675c1153af40cba25021b211b0
kemylima <kemilly.gomes.lima@escola.pr.gov.br>
2025-05-12 15:02:33 +0000

    ok

---
+++ perguntas.js
@@ -0,0 +1,15 @@
+criaCartao(
+'promagração'  ,
+'O que JavaScript',
+'Linguagem de promagração'
+)
+criaCartao(
+'inglês',
+'como se escreve dia do pagamento?',
+'Pay day'
+)
+criaCartao(
+'Matemática',
+'Qual gráfico da função quadrática?',
+'Parábola'
+)
