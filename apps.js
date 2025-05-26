c24650848390e9a40ba10c233eec7e2d5f041d7e
kemylima <kemilly.gomes.lima@escola.pr.gov.br>
2025-05-26 14:59:07 +0000

    ok

---
+++ apps.js
@@ -0,0 +1,7 @@
+function criaCartao(categoria, pergunta,resposta){
+    let container = Document.getElementByid('container')
+    let cartao = document.createElement('article')
+    cartao.className='cartao'
+
+
+}
